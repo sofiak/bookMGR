17191191d40922aa5922b71641b159493baa998b
014289985 <sofia.kansikas@gmail.com>
2015-09-12 22:54:02 +0300

    DB

--- bookMGR/src/main/java/com/bookmgr/Main.java
+++ bookMGR/src/main/java/com/bookmgr/Main.java
@@ -4,7 +4,7 @@ import org.javalite.activejdbc.Base;
  
 public class Main {
     public static void main(String[] args) {
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://52.16.13.120/test", "sofia", "iambatgirl");
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://52.16.13.120/bookMGR", "sofia", "iambatgirl");
         Base.close();
     }
    
