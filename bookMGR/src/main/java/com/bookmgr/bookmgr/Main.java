46843b1de7d48f34d356fd3487f2532c68486cb6
014289985 <sofia.kansikas@gmail.com>
2015-09-12 19:28:16 +0300

    muokkaus

--- bookMGR/src/main/java/com/bookmgr/bookmgr/Main.java
+++ bookMGR/src/main/java/com/bookmgr/bookmgr/Main.java
@@ -1,6 +1,11 @@
 package com.bookmgr.bookmgr;
 
+import org.javalite.activejdbc.Base;
 
 public class Main {
+    public static void Main() {
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://52.16.13.120/test", "sofia", "iambatgirl");
+        Base.close();
+    }
     
 }
