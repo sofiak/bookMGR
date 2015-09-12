d3e2671e21ef27c4fa16baa77611564e0d7927e3
014289985 <sofia.kansikas@gmail.com>
2015-09-12 22:46:25 +0300

    models

---
+++ bookMGR/src/main/java/com/bookmgr/models/Reservation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookmgr.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("reservations")
+public class Reservation extends Model{
+    
+}
---
+++ bookMGR/src/main/java/com/bookmgr/models/User.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookmgr.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("users")
+
+public class User extends Model{
+    
+}
